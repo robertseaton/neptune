cb63ec35c65d582d3b913d5cf720441e256a3140
Austin Walters <lettergramm@gmail.com>
2013-12-26 19:17:57 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
-	"fmt"
+//	"fmt"
 	"io/ioutil"
+	"net/http"
+	"html/template"
 )
 
 type Page struct {
 	Title string
-	Body []byte
+	Body  template.HTML
 }
 
-// Save a webpage.
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: template.HTML(body)}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/"):]
+	p, err := loadPage(title)
+
+	if err != nil {
+		http.Redirect(w, r, "/index", http.StatusFound)
+		return
+	}
+
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
+}
+
+func main() {
+	http.HandleFunc("/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
